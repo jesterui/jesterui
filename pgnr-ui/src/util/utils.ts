9221d143fdb3351ba84b89dde34fa53c90d81532
theborakompanioni <theborakompanioni+github@gmail.com>
2022-04-19 01:27:59 +0200

    add throttle function to utils

--- pgnr-ui/src/util/utils.ts
+++ pgnr-ui/src/util/utils.ts
@@ -14,3 +14,25 @@ export const debounce = (fn: Function, ms = 255) => {
     timeoutId = setTimeout(() => fn.apply(this, args), ms)
   }
 }
+
+export const throttle = (fn: Function, threshold = 2500, scope: unknown = undefined) => {
+  let last: number
+  let deferTimer: ReturnType<typeof setTimeout>
+
+  return (...args: unknown[]) => {
+    const context = scope || this
+
+    const now = Date.now()
+    const shouldDefer = last && now < last + threshold
+    if (!shouldDefer) {
+      last = now
+      fn.apply(context, args)
+    } else {
+      clearTimeout(deferTimer)
+      deferTimer = setTimeout(() => {
+        last = now
+        fn.apply(context, args)
+      }, threshold)
+    }
+  }
+}
