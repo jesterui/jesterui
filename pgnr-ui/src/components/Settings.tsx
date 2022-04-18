cde4090fbdcce099ad00b4557b854c0d2eb5a216
theborakompanioni <theborakompanioni+github@gmail.com>
2022-04-18 23:47:47 +0200

    feat: enable input of keypair

--- pgnr-ui/src/components/Settings.tsx
+++ pgnr-ui/src/components/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback, ChangeEvent } from 'react'
 import { useSettings, useSettingsDispatch } from '../context/SettingsContext'
 import * as Nostr from '../util/nostr/identity'
 import { ActivityIndicator } from '../components/ActivityIndicator'
@@ -19,6 +19,8 @@ import Checkbox from '@material-tailwind/react/Checkbox'
 import Heading1 from '@material-tailwind/react/Heading1'
 // @ts-ignore
 import Heading2 from '@material-tailwind/react/Heading2'
+// @ts-ignore
+import Input from "@material-tailwind/react/Input";
 
 const developmentRelays = ['ws://localhost:7000']
 
@@ -39,6 +41,9 @@ const publicRelays = [
 ]
 
 const defaultRelays = [...(process.env.NODE_ENV === 'development' ? developmentRelays : []), ...publicRelays]
+
+type PubKey = string | null
+type PrivKey = string | null
 
 function TestNostrConnectionButton() {
   const incomingNostrBuffer = useIncomingNostrEventsBuffer()
@@ -107,20 +112,30 @@ function TestNostrConnectionButton() {
       return
     }
 
-    const publicKey = publicKeyOrNull!
-    const privateKey = privateKeyOrNull!
+    try {
+      const publicKey = publicKeyOrNull!
+      const privateKey = privateKeyOrNull!
 
-    const eventParts = NostrEvents.blankEvent()
-    eventParts.kind = 1 // text_note
-    eventParts.pubkey = publicKey
-    eventParts.created_at = Math.floor(Date.now() / 1000)
-    eventParts.content = 'Hello World'
-    const event = NostrEvents.constructEvent(eventParts)
-    const signedEvent = await NostrEvents.signEvent(event, privateKey)
+      const eventParts = NostrEvents.blankEvent()
+      eventParts.kind = 1 // text_note
+      eventParts.pubkey = publicKey
+      eventParts.created_at = Math.floor(Date.now() / 1000)
+      eventParts.content = 'Hello World'
+      const event = NostrEvents.constructEvent(eventParts)
+      const signedEvent = await NostrEvents.signEvent(event, privateKey)
 
-    setWaitForEvent(signedEvent)
+      const isSignatureValid = await NostrEvents.verifySignature(signedEvent)
+      if (!isSignatureValid) {
+        throw new Error('Cannot create valid signature for Nostr event..')
+      }
 
-    outgoingNostr.emit(NIP01.ClientEventType.EVENT, NIP01.createClientEventMessage(signedEvent))
+      setWaitForEvent(signedEvent)
+
+      outgoingNostr.emit(NIP01.ClientEventType.EVENT, NIP01.createClientEventMessage(signedEvent))
+    } catch(e) {
+      const message = e instanceof Error && e.message || e
+      window.alert(`Error: ${message}`)
+    }
   }
 
   return (
@@ -130,14 +145,45 @@ function TestNostrConnectionButton() {
   )
 }
 
+const validateKeyPair = async (pubKey: PubKey, privKey: PrivKey): Promise<boolean> => {
+  if (pubKey === null || privKey === null) return false
+
+  try {
+    const eventParts = NostrEvents.blankEvent()
+    eventParts.kind = 1 // text_note
+    eventParts.pubkey = pubKey
+    eventParts.created_at = Math.floor(Date.now() / 1000)
+    eventParts.content = 'Hello World'
+    const event = NostrEvents.constructEvent(eventParts)
+    const signedEvent = await NostrEvents.signEvent(event, privKey)
+
+    const isSignatureValid = await NostrEvents.verifySignature(signedEvent)
+    return isSignatureValid
+  } catch (e) {
+    return false
+  }
+}
+
+const useForceRerender = () => {
+  const [tick, setTick] = useState(0)
+  const update = useCallback(() => {
+    setTick((tick) => tick + 1)
+  }, [tick])
+  return update
+};
+
 export default function Settings() {
+  const forceRerender = useForceRerender()
   const settings = useSettings()
   const settingsDispatch = useSettingsDispatch()
-  const outgoingNostr = useOutgoingNostrEvents()
   const websocket = useWebsocket()
 
   const publicKeyOrNull = settings.identity?.pubkey || null
   const privateKeyOrNull = getSession()?.privateKey || null
+
+  const [publicKeyInputValue, setPublicKeyInputValue] = useState<PubKey>(publicKeyOrNull)
+  const [privateKeyInputValue, setPrivateKeyInputValue] = useState<PrivKey>(privateKeyOrNull)
+  const [keyPairValid, setKeyPairValid] = useState<boolean | undefined>(undefined)
 
   const [selectedBot, setSelectedBot] = useState<SelectedBot>(
     (() => {
@@ -156,6 +202,38 @@ export default function Settings() {
     setSelectedBot(selectedBot)
     settingsDispatch({ ...settings, botName: bot?.name || null })
   }
+
+  useEffect(() => {
+    setPublicKeyInputValue(publicKeyOrNull)
+  }, [publicKeyOrNull])
+
+  useEffect(() => {
+    setPrivateKeyInputValue(privateKeyOrNull)
+  }, [privateKeyOrNull])
+
+  const updatePrivKey = (privKey: PrivKey) => {
+    setSessionAttribute({ privateKey: privKey })
+    forceRerender()
+  }
+
+  const updatePubKey = useCallback((pubKey: PubKey) => {
+    if(pubKey === null) {
+      settingsDispatch({...settings, identity: undefined})
+    } else {
+      settingsDispatch({...settings, identity: { ...settings.identity, pubkey: pubKey }})
+    }
+  }, [settings])
+
+  useEffect(() => {
+    validateKeyPair(publicKeyInputValue, privateKeyInputValue)
+      .then((success) => {
+        setKeyPairValid(success)
+        if(success) {
+          updatePrivKey(privateKeyInputValue)
+          updatePubKey(publicKeyInputValue)
+        }
+      })
+  }, [publicKeyInputValue, privateKeyInputValue])
 
   const onRelayClicked = (relay: string) => {
     const index = settings.relays.indexOf(relay, 0)
@@ -194,16 +272,36 @@ export default function Settings() {
   }
 
   return (
-    <div className="screen-settings">
+    <div className="screen-settings pb-4">
       <Heading1 color="blueGray">Settings</Heading1>
 
       <Heading2 color="blueGray">Identity</Heading2>
       <div>
-        <div className="py-1">
-          Public Key: <span className="font-mono">{settings.identity?.pubkey}</span>
+        <div className="pb-4">
+        <div className="pb-2">
+          Public Key: <span className="font-mono">{publicKeyOrNull}</span>
+          <Input
+            type="text"
+            size="regular"
+            outline={true}
+            value={publicKeyInputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPublicKeyInputValue(e.target.value)}
+            placeholder="Public Key"
+        />
         </div>
-        <div className="py-1">
+        <div className="pb-2">
           Private Key: <span className="font-mono">{privateKeyOrNull}</span>
+          <Input
+            type="text"
+            size="regular"
+            outline={true}
+            value={privateKeyInputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPrivateKeyInputValue(e.target.value)}
+            placeholder="Private Key"
+            error={keyPairValid === false ? true : undefined}
+            success={keyPairValid === true ? true : undefined}
+        />
+        </div>
         </div>
         <div className="py-1">
           <button type="button" className="bg-white bg-opacity-20 rounded px-2 py-1" onClick={newIdentityButtonClicked}>
@@ -229,41 +327,43 @@ export default function Settings() {
       </div>
 
       <Heading2 color="blueGray">Relays</Heading2>
-      <div>
-        Status:
-        <span className="px-1">
-          <WebsocketIndicator />
-        </span>
-        <span className="font-mono">{readyStatePhrase(websocket?.readyState)}</span>
-      </div>
-      <div>
-        Host:
-        <span className="px-1">
-          <span className="font-mono">{websocket?.url}</span>
-        </span>
-      </div>
-      <div className="py-1">
-        <TestNostrConnectionButton />
-      </div>
-      <div className="py-2">
-        {defaultRelays.map((relay, index) => (
-          <div key={index}>
-            <Checkbox
-              color="blueGray"
-              text={
-                <div>
-                  <ActivityIndicator
-                    isOn={!!(websocket?.url.startsWith(relay) && websocket?.readyState === WebSocket.OPEN)}
-                  />{' '}
-                  {relay}
-                </div>
-              }
-              id={relay}
-              checked={settings.relays.includes(relay)}
-              onChange={() => onRelayClicked(relay)}
-            />
-          </div>
-        ))}
+      <div className="pb-4">
+        <div className="pb-1">
+          Status:
+          <span className="px-1">
+            <WebsocketIndicator />
+          </span>
+          <span className="font-mono">{readyStatePhrase(websocket?.readyState)}</span>
+        </div>
+        <div className="pb-1">
+          Host:
+          <span className="px-1">
+            <span className="font-mono">{websocket?.url}</span>
+          </span>
+        </div>
+        <div className="py-1">
+          <TestNostrConnectionButton />
+        </div>
+        <div className="py-1">
+          {defaultRelays.map((relay, index) => (
+            <div key={index}>
+              <Checkbox
+                color="blueGray"
+                text={
+                  <div>
+                    <ActivityIndicator
+                      isOn={!!(websocket?.url.startsWith(relay) && websocket?.readyState === WebSocket.OPEN)}
+                    />{' '}
+                    {relay}
+                  </div>
+                }
+                id={relay}
+                checked={settings.relays.includes(relay)}
+                onChange={() => onRelayClicked(relay)}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
