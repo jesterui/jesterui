1953ca81b93aefeb6d197f7d11152d94d7589bdd
theborakompanioni <theborakompanioni+github@gmail.com>
2022-05-08 18:00:49 +0200

    ui: improve search page and hint for typos

--- pgnr-ui/src/components/SearchPage.tsx
+++ pgnr-ui/src/components/SearchPage.tsx
@@ -3,26 +3,92 @@ import { useNavigate } from 'react-router-dom'
 
 import { useIncomingNostrEvents } from '../context/NostrEventsContext'
 
+import JesterId from '../components/JesterId'
+
 import * as JesterUtils from '../util/jester'
 
 // @ts-ignore
 import Heading1 from '@material-tailwind/react/Heading1'
+// @ts-ignore
+import Heading4 from '@material-tailwind/react/Heading4'
 // @ts-ignore
 import Heading6 from '@material-tailwind/react/Heading6'
 // @ts-ignore
 import Input from '@material-tailwind/react/Input'
 // @ts-ignore
 import Button from '@material-tailwind/react/Button'
-import JesterId from './JesterId'
+// @ts-ignore
+import Small from '@material-tailwind/react/Small'
+
+interface SearchFrom {
+  onSearchButtonClicked: (val: string) => void
+  onSearchInputChange?: (val: string) => void
+  error?: boolean
+  success?: boolean
+}
+const SearchFrom = (props: SearchFrom) => {
+  const [searchInputValue, setSearchInputValue] = useState<string>('')
+
+  const onSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value
+    setSearchInputValue(searchTerm)
+
+    if (props.onSearchInputChange) {
+      props.onSearchInputChange(searchTerm)
+    }
+  }
+
+  return (
+    <form noValidate onSubmit={() => props.onSearchButtonClicked(searchInputValue)}>
+      <div className="pb-2 grow">
+        <div className="flex justify-center">
+          <div className="w-full">
+            <Input
+              type="text"
+              size="lg"
+              outline={true}
+              value={searchInputValue}
+              onChange={onSearchInputChange}
+              placeholder="Search"
+              style={{ color: 'currentColor' }}
+              error={props.error ? ' ' : undefined}
+              success={props.success === true ? ' ' : undefined}
+            />
+          </div>
+
+          <div className="ml-1">
+            <Button
+              className="h-12"
+              type="submit"
+              color="deepOrange"
+              buttonType="filled"
+              size="lg"
+              rounded={false}
+              block={false}
+              iconOnly={false}
+              ripple="light"
+            >
+              Search
+            </Button>
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+}
 
 export default function SearchPage() {
   const navigate = useNavigate()
   const incomingNostr = useIncomingNostrEvents()
-  const [searchInputValue, setSearchInputValue] = useState<string>('')
   const [searchResults, setSearchResults] = useState<string[] | null>(null)
+  const [inputLooksLikeJesterId, setInputLooksLikeJesterId] = useState<boolean | null>(null)
   const [inputIsJesterId, setInputIsJesterId] = useState<boolean | null>(null)
 
   const search = (searchInput: string) => {
+    setInputLooksLikeJesterId(false)
+    setInputIsJesterId(false)
+    setSearchResults(null)
+
     if (!searchInput) {
       setSearchResults([])
       return
@@ -31,35 +97,35 @@ export default function SearchPage() {
     // currently, the search value must be a jester id
     const indexOfPrefix = searchInput.indexOf(JesterUtils.JESTER_ID_PREFIX + '1')
     if (indexOfPrefix < 0) {
-      setInputIsJesterId(false)
       setSearchResults([])
       return
     }
 
     // try finding a jesterId in the input, e.g. might be an url "https://example.com/jester1abcdef123..."
     const possibleJesterId = searchInput.substring(indexOfPrefix)
-    console.debug(`Found possible jesterId: ${possibleJesterId}`)
+
+    setInputLooksLikeJesterId(
+      possibleJesterId.length > JesterUtils.VALID_JESTER_ID_EXAMPLE.length - 10 &&
+        possibleJesterId.length < JesterUtils.VALID_JESTER_ID_EXAMPLE.length + 10
+    )
 
     const jesterId = JesterUtils.tryParseJesterId(possibleJesterId as JesterUtils.JesterId)
     if (jesterId === null) {
       console.warn('Could not parse jesterId from search input value')
       setSearchResults([])
-      setInputIsJesterId(false)
       return
     }
 
-    // at the moment, just redirect to the game - may not exist, but thats fine for now
+    setInputLooksLikeJesterId(true)
     setInputIsJesterId(true)
     setSearchResults(null)
+
+    // at the moment, just redirect to the game - may not exist, but thats fine for now
     navigate(`/redirect/game/${jesterId}`)
   }
 
-  const onSearchButtonClicked = () => {
-    search(searchInputValue)
-  }
-
-  const onSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchInputValue(e.target.value)
+  const onSearchInputChange = () => {
+    setInputLooksLikeJesterId(null)
     setInputIsJesterId(null)
     setSearchResults(null)
   }
@@ -73,52 +139,53 @@ export default function SearchPage() {
           ) : (
             <>
               <div className="w-full grid grid-cols-1">
-                <div className="flex justify-center">{<Heading1 color="blueGray">chess on nostr</Heading1>}</div>
-                <form noValidate onSubmit={() => onSearchButtonClicked()}>
-                  <div className="pb-2 grow">
-                    <div className="flex justify-center">
-                      <div className="w-full">
-                        <Input
-                          type="text"
-                          size="lg"
-                          outline={true}
-                          value={searchInputValue}
-                          onChange={(e: ChangeEvent<HTMLInputElement>) => onSearchInputChange(e)}
-                          placeholder="Search"
-                          style={{ color: 'currentColor' }}
-                          error={inputIsJesterId === false ? ' ' : undefined}
-                          success={inputIsJesterId === true ? ' ' : undefined}
-                        />
-                      </div>
-                    </div>
-                  </div>
+                <div className="flex justify-center">
+                  <Heading1 color="blueGray">Search</Heading1>
+                </div>
 
-                  <div className="flex justify-center items-center">
-                    <Button
-                      type="submit"
-                      color="teal"
-                      buttonType="filled"
-                      size="lg"
-                      rounded={false}
-                      block={false}
-                      iconOnly={false}
-                      ripple="light"
-                    >
-                      Search
-                    </Button>
+                <div className="flex">
+                  <div className="mx-1">
+                    <Small color="">
+                      Search for games, <span style={{ textDecoration: 'line-through' }}>players and other stuff.</span>
+                    </Small>
+                    <Small color="orange"> not quite yet... Coming soon!</Small>
                   </div>
-                </form>
-                <div className="pb-2 grow">
+                </div>
+
+                <SearchFrom
+                  onSearchButtonClicked={search}
+                  onSearchInputChange={onSearchInputChange}
+                  error={inputIsJesterId === false ? true : undefined}
+                  success={inputIsJesterId === true ? true : undefined}
+                />
+
+                <div className="pb-2 grow my-4">
                   {searchResults?.length === 0 && (
                     <>
-                      <Heading6 color="blueGray">No results found. </Heading6>
-                      <p>Are you sure this is a game id?</p>
-                      <p>
-                        <small>
-                          e.g. a game id looks like this:{' '}
-                          <JesterId jesterId="jester13s8c6xzp33n93zn2qmvtgaypncphz585fggggnzvppmxnyamvc4qpu3sdv" />
-                        </small>
-                      </p>
+                      <Heading4 color="blueGray">No results found. </Heading4>
+                      {inputLooksLikeJesterId ? (
+                        <>
+                          <Heading6 color="blueGray">Damn... there must be a typo somewhere... : (</Heading6>
+                          <p>
+                            It kind of looks like a{' '}
+                            <code className="border border-solid border-blue-gray-500 text-xs font-semibold mx-1 px-2.5 py-1 rounded">
+                              jesterId
+                            </code>
+                            , but it contains errors.
+                          </p>
+                          <p>If you got this ID from another player, please verify the value and try again.</p>
+                        </>
+                      ) : (
+                        <>
+                          <p>Are you sure this is a valid game id?</p>
+                          <small>
+                            e.g. a game id looks like this:
+                            <code className="text-xs font-semibold px-2.5 py-1 rounded">
+                              <JesterId jesterId={JesterUtils.VALID_JESTER_ID_EXAMPLE} />
+                            </code>
+                          </small>
+                        </>
+                      )}
                     </>
                   )}
                 </div>
--- pgnr-ui/src/util/jester.ts
+++ pgnr-ui/src/util/jester.ts
@@ -8,6 +8,8 @@ import { ChessInstance } from '../components/ChessJsTypes'
 import { Buffer } from 'buffer'
 import { bech32m, bech32 } from 'bech32'
 
+export const VALID_JESTER_ID_EXAMPLE = 'jester1ncmkasntavrcj8ujv32a98236kgnx5a3cm9cl9kmqpjh0tgyg46qqsfhdp'
+
 export const FEN_START_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 export const JESTER_START_GAME_E_REF = bytesToHex(sha256(FEN_START_POSITION))
 
