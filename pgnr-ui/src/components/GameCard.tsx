6b66a7b1f2d411c7a6ef9f0758539e0aeb930d66
theborakompanioni <theborakompanioni+github@gmail.com>
2022-05-14 14:48:02 +0200

    chore: remove unused import

--- pgnr-ui/src/components/GameCard.tsx
+++ pgnr-ui/src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef, useMemo } from 'react'
+import React, { useCallback, useRef, useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
 import { AppSettings, useSettings, useSettingsDispatch } from '../context/SettingsContext'
