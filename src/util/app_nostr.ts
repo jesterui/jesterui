01bce304040f2fa63ce9fb28ef5819d2c69ad939
theborakompanioni <theborakompanioni+github@gmail.com>
2022-12-20 19:24:33 +0100

    chore: remove dead relays

--- src/util/app_nostr.ts
+++ src/util/app_nostr.ts
@@ -2,18 +2,22 @@ const developmentRelays = ['ws://localhost:7000', 'wss://non-existing-nostr-rela
 
 const publicRelays = [
   'wss://nostr-pub.wellorder.net',
-  'wss://relayer.fiatjaf.com',
+  'wss://relay.damus.io',
+  'wss://relay.nostr.info',
   'wss://nostr.rocks',
-  'wss://rsslay.fiatjaf.com',
-  'wss://freedom-relay.herokuapp.com/ws',
-  'wss://nostr-relay.freeberty.net',
-  'wss://nostr.bitcoiner.social',
   'wss://nostr-relay.wlvs.space',
-  'wss://nostr.onsats.org',
   'wss://nostr-relay.untethr.me',
   'wss://nostr-verified.wellorder.net',
-  'wss://nostr.drss.io',
-  'wss://nostr.unknown.place',
+  // inactive relays (last checked on 2022-12-20):
+  // 'wss://nostr.zebedee.cloud',
+  // 'wss://relayer.fiatjaf.com',
+  // 'wss://rsslay.fiatjaf.com',
+  // 'wss://freedom-relay.herokuapp.com/ws',
+  // 'wss://nostr-relay.freeberty.net',
+  // 'wss://nostr.bitcoiner.social',
+  // 'wss://nostr.onsats.org',
+  // 'wss://nostr.drss.io',
+  // 'wss://nostr.unknown.place',
 ]
 
 export const DEFAULT_RELAYS = [...(process.env.NODE_ENV === 'development' ? developmentRelays : []), ...publicRelays]
